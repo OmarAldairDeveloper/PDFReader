684878524837386d63f116fbac689606d61c2135
Omar Aldair <omaraldair.developer@gmail.com>
2019-01-21 13:42:46 -0600

    Hemos agregado comentarios al codigo

--- SMPDF/ViewController.swift
+++ SMPDF/ViewController.swift
@@ -14,15 +14,13 @@ import SafariServices
 class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelegate {
     
     let pdfView = PDFView()
-    /*var currentIndex = 0
-    var textToFind = ""*/
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         pdfView.translatesAutoresizingMaskIntoConstraints = false
-        
         view.addSubview(pdfView)
         
         //Restricciones para que ocupe todo el espacio de pantalla la PDFView
@@ -41,7 +39,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
         // Agregar PDFViewDelegate
         self.pdfView.delegate = self
         
-        
+        // Botones de la navigationItem
         let search = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchText))
         let action = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareSelection(sender:)))
         let next = UIBarButtonItem(barButtonSystemItem: .fastForward, target: self.pdfView, action: #selector(PDFView.goToNextPage(_:)))
@@ -61,12 +59,16 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
     
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
         
+        // Si lo que hemos arrastrado es un PDF, entonces entrar a la condición
         if session.hasItemsConforming(toTypeIdentifiers: [kUTTypePDF as String]){
             
+            // Cargar los objetos que hemos arrastrado en forma de PDFDocumentClass
             session.loadObjects(ofClass: PDFDocumentClass.self) { (items) in
             
+                // Obtener el primer objeto arrastrado
                 guard let pdfDoc = items.first as? PDFDocumentClass else { return }
                 
+                // Obtener la data de ese documento y así podemos contruir el PDFDocument
                 if let data = pdfDoc.data{
                     self.openPDF(data: data)
                 }
@@ -81,6 +83,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
     
     // MARK: PDFViewDelegate
     func pdfViewWillClick(onLink sender: PDFView, with url: URL) {
+        // Abrir Safari al clickear en un vínculo dentro del pdf
         let safariController = SFSafariViewController(url: url)
         safariController.modalPresentationStyle = .formSheet
         present(safariController, animated: true)
@@ -88,7 +91,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
     
     // MARK: Manage PDF
     func openPDF(data: Data){
-        
+        // Crear el PDFDocument y asignarlo a la PDFView
         if let document = PDFDocument(data: data){
             self.pdfView.document = document
             self.pdfView.goToFirstPage(nil)
@@ -98,6 +101,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
     
     @objc func searchText(){
         
+        // Crear un alert controller con un textfield para buscar palabras dentro del documento
         let alert = UIAlertController(title: "Buscar texto", message: "", preferredStyle: .alert)
         
         alert.addTextField { (txtField) in
@@ -106,10 +110,11 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
         
         alert.addAction(UIAlertAction(title: "Buscar", style: .default, handler: { (action) in
             guard let text = alert.textFields?.first?.text else { return }
-            /*guard let match = self.pdfView.document?.findString(text, fromSelection: self.pdfView.highlightedSelections?.first, withOptions: .caseInsensitive) else { return }*/
             
+            // Coincidencias
             guard let matches = self.pdfView.document?.findString(text, withOptions: .caseInsensitive) else { return }
             
+            // Recorrer coincidencias y luego sus páginas para colorear esas coincidencias de color rojo
             matches.forEach({ (match) in
                 match.pages.forEach({ (page) in
                     let highlight = PDFAnnotation(bounds: match.bounds(for: page), forType: .highlight, withProperties: nil)
@@ -120,13 +125,11 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
                 })
             })
             
+            // Ir al primer match
             if let firstMatch = matches.first{
                 self.pdfView.go(to: firstMatch)
             }
             
-            /*match.color = UIColor.red
-            self.pdfView.go(to: match)
-            self.pdfView.highlightedSelections = [match]*/
 
    
         }))
@@ -141,6 +144,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
     
     @objc func shareSelection(sender: UIBarButtonItem){
         
+        
         guard let selection = self.pdfView.currentSelection?.attributedString else{
             let alert = UIAlertController(title: "No hay nada seleccionado", message: "Selecciona una parte del PDF", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -148,6 +152,7 @@ class ViewController: UIViewController, UIDropInteractionDelegate, PDFViewDelega
             return
         }
         
+        // Compartir texto seleccionado a través de otras aplicaciones
         let sharedVC = UIActivityViewController(activityItems: [selection], applicationActivities: nil)
         sharedVC.popoverPresentationController?.barButtonItem = sender
         present(sharedVC, animated: true)
